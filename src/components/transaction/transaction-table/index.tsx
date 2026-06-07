0660cb5b3e40b8d93487f291777fe3908fab8575
Amna <lamna160304@gmail.com>
2026-06-07 10:11:34 -0700

    fix: remove unused _TransactionType import

--- src/components/transaction/transaction-table/index.tsx
+++ src/components/transaction/transaction-table/index.tsx
@@ -1,6 +1,6 @@
 import { DataTable } from "@/components/data-table";
 import { transactionColumns } from "./column";
-import { _TRANSACTION_TYPE, _TransactionType } from "@/constant";
+import { _TRANSACTION_TYPE } from "@/constant";
 import { useState } from "react";
 import useDebouncedSearch from "@/hooks/use-debounce-search";
 import { useFormatCurrency } from "@/hooks/use-format-currency";
